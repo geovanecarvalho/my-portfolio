a0a575ec26ea85374f2ba2e669e12d233e372465
geovanecarvalho <geovaneoliveira.75@hotmail.com>
2025-05-30 19:33:13 -0300

    new config vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/', // <- importante
+  base: '/my-portfolio/', // <- coloque exatamente o nome do repositório aqui
   plugins: [react()],
 })
+
