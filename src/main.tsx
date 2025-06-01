df30643dacc9450f6df7cd2f128db501bc3a5ed6
geovanecarvalho <geovaneoliveira.75@hotmail.com>
2025-05-31 23:52:22 -0300

    config hashrouter github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import './index.css'; // ou './style.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter} from 'react-router-dom'
 import App from './App.tsx'
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="my-portfolio">
+    <HashRouter>
         <App/>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
 
