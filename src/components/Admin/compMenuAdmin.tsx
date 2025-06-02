efaef3178652dee01abd2218f60ac453d04a5d15
geovanecarvalho <geovaneoliveira.75@hotmail.com>
2025-06-02 20:16:44 -0300

    add flex w-full justify-between

--- src/components/Admin/compMenuAdmin.tsx
+++ src/components/Admin/compMenuAdmin.tsx
@@ -20,7 +20,7 @@ const ComponentMenuAdmin = ({ pageName }: Props) => {
 
   return (
     <nav className="bg-gradient-to-r from-blue-900 via-blue-700 to-blue-400 text-white p-4 relative">
-      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+      <div className="flex justify-between items-center w-full px-4 py-3">
         <div className="flex items-center text-white text-xl font-bold space-x-2">
           <img src={Logo} alt="Logo" className="w-10" />
           <span>{pageName}</span>
